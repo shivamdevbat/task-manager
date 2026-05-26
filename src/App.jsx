44017fd730ec60db24423787ea76244638159880
shivamdevbat <shivamdwivedi1211@gmail.com>
2026-05-26 14:40:47 +0530

    added todo feature with delete

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,51 @@
 import Heading from "./components/heading";
 import Taskbar from "./components/taskbar";
+import { useState } from "react";
+import { MdDeleteOutline } from "react-icons/md";
+import { FaRegEdit } from "react-icons/fa";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [tasks, setTask] = useState([]);
+
+  function addTask() {
+    if (!input.trim()) return;
+
+    setTask([...tasks, { id: Date.now(), title: input, label: "" }]);
+    setInput("");
+  }
+
+  function deleteTask(id) {
+    setTask(tasks.filter((task) => task.id !== id));
+  }
   return (
     <main className="bg-zinc-950 min-h-screen flex justify-center items-center">
       <div className="flex flex-col justify-between w-full max-w-md min-h-175 bg-zinc-700 border rounded-xl">
         <div className="flex-1 p-5">
           <Heading />
         </div>
-
+        {tasks.map((task) => (
+          <div>
+            <p
+              key={task.id}
+              className={`flex justify-between text-white text-sm mt-2 p-2 pl-6  border-b border-zinc-500 mx-5 `}
+            >
+              {task.title}{" "}
+              <div className="flex gap-2">
+                <span>
+                  <FaRegEdit />
+                </span>
+                <span onClick={() => deleteTask(task.id)} className="cursor-pointer">
+                  <MdDeleteOutline />
+                </span>
+              </div>
+            </p>
+          </div>
+        ))}
         <div className="border-t border-zinc-500"></div>
 
         <div className="p-5">
-          <Taskbar />
+          <Taskbar input={input} setInput={setInput} addTask={addTask} />
         </div>
       </div>
     </main>
--- src/components/taskbar.jsx
+++ src/components/taskbar.jsx
@@ -1,11 +1,21 @@
-export default function Taskbar() {
+export default function Taskbar({ input, setInput, addTask }) {
   return (
-    <div className="flex items-center gap-2 w-full">
-      <input
-        className="flex-1 border px-4 py-2.5 pl-6  bg-zinc-600 rounded-xl text-sm"
-        placeholder="Add a new task......"
-      />
-      <button className="bg-emerald-600 text-white px-4 py-2 rounded-xl">+</button>
+    <div>
+      <div className="flex items-center gap-2 w-full">
+        <input
+          className="flex-1 border px-4 py-2.5 pl-6  bg-zinc-600 rounded-xl text-sm"
+          placeholder="Add a new task......"
+          value={input}
+          onChange={(e) => {
+            setInput(e.target.value);
+            console.log(e.target.value);
+          }}
+          onKeyDown={(e) => e.key === "Enter" && addTask()}
+        />
+        <button className="bg-emerald-600 text-white px-4 py-2 rounded-xl" onClick={addTask}>
+          +
+        </button>
+      </div>
     </div>
   );
 }
